c71f5576734db766bf7c902e555318a227e8fb8f
Leone de Souza <leosouza@rd.com.br>
2024-03-01 23:40:21 -0300

    ✨ feat(front): Add client entry point
    ✨ Introduce client entry point file:
    - front/src/client/index.ts
    
    🔧 This commit sets up the client entry point for the front-end application

---
+++ front/src/client/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios"
+import { User } from "../state-managment/utils"
+
+const makeClient = (url: string) => {
+    return {
+        user: {
+            create: async(body: {}):Promise<User> => {
+                const res = await axios.post(`${url}/users`, body)
+                return res.data
+            },
+            list: async ():Promise<User[]> => {
+                const res = await axios.get(`${url}/users`)
+                return res.data
+            },
+            update: async (id: string, body: {}):Promise<User> => {
+                const res = await axios.put(`${url}/users/${id}`, body)
+                return res.data
+            },
+            delete: async (id: string):Promise<User> => {
+                const res = await axios.delete(`${url}/users/${id}`)
+                return res.data
+            }
+        }
+    }
+
+}
+
+
+export default makeClient
